e63fcaa03de046f00f02682d15ea442e2696a039
Benjamin Earley <BenjaminEarley92@gmail.com>
2015-06-15 22:53:16 -0400

    Initial commit

---
+++ app/src/main/java/com/benjaminearley/spotifystreamer/MainActivityFragment.java
@@ -0,0 +1,55 @@
+package com.benjaminearley.spotifystreamer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivityFragment extends Fragment {
+
+    private static final int DATASET_COUNT = 60;
+
+    protected RecyclerView mRecyclerView;
+    protected ListAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected String[] mDataset;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initDataset();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new ListAdapter(mDataset);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
+}
