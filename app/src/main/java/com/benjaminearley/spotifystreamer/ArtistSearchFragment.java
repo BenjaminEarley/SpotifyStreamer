8d7139883774d74161a1f04e2a55a199f68599db
Benjamin Earley <BenjaminEarley92@gmail.com>
2015-06-23 22:34:03 -0400

    Updates to artist cards. Created Top Song fragment and views

--- app/src/main/java/com/benjaminearley/spotifystreamer/ArtistSearchFragment.java
+++ app/src/main/java/com/benjaminearley/spotifystreamer/ArtistSearchFragment.java
@@ -1,7 +1,7 @@
 package com.benjaminearley.spotifystreamer;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class MainActivityFragment extends Fragment {
+public class ArtistSearchFragment extends Fragment {
 
     private static final int DATASET_COUNT = 60;
 
@@ -18,7 +18,7 @@ public class MainActivityFragment extends Fragment {
     protected RecyclerView.LayoutManager mLayoutManager;
     protected String[] mDataset;
 
-    public MainActivityFragment() {
+    public ArtistSearchFragment() {
     }
 
     @Override
